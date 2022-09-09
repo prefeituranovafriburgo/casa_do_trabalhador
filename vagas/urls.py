a21e79a46a5fa247bbfa052148d4b5b0e7ade6da
Luis Eduardo Cordeiro Salarini <eduardo.pmnf@gmail.com>
2022-09-09 15:24:01 -0300

    att

--- vagas/urls.py
+++ vagas/urls.py
@@ -26,9 +26,11 @@ urlpatterns = [
     path('vagas/table/', views.vagas_table, name='vagas_table'),    
     path('get_vaga/', views.get_cargo, name='get_vaga' ),
     path('get_empresa/', views.get_empresa, name='get_empresa' ),
+    path('get_candidatos/', views.get_candidatos, name='get_candidatos' ),
     path('logout/', views.sair, name='logout'),
     path('login/', views.login_view, name='login'),
     path('visualizar-vaga/alt0x#<id>001/listar-canditados/', views.candidatosporvaga, name='listar_candidatos'),
     path('vagas-com-candidatos/', views.vagascomcandidatos, name='vagas_com_candidatos'),
-    path('gambiarra/', views.gambiarra, name='gambiarra'),    
+    path('pesquisar-candidatos/', views.pesquisar_candidatos, name='pesquisar_candidatos'), 
+    path('visualizar-candidatos/<id>', views.visualizar_candidato, name='visualizar_candidato'), 
 ]
