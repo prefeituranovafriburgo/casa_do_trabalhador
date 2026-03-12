7dc5fc38df33e60bc33c68fb223e2d72f4befa4a
Luisa de Andrade Lacave <Luisa@PMNF-CPD001.pmnfgov.nf>
2026-03-12 14:50:24 -0300

    Update encaminhamento

--- vagas/templatetags/qntvagas.py
+++ vagas/templatetags/qntvagas.py
@@ -9,3 +9,23 @@ def qntcandidatos(obj):
         return str(len(candidatos))
     except:
         return None
+
+@register.filter
+def mascarar_cpf(cpf):
+    """Exibe apenas os 3 primeiros dígitos do CPF, mascarando o restante."""
+    if not cpf:
+        return ''
+    digits = ''.join(c for c in str(cpf) if c.isdigit())
+    if len(digits) >= 3:
+        return digits[:3] + '.XXX.XXX-XX'
+    return str(cpf)
+
+@register.filter
+def mascarar_cnpj(cnpj):
+    """Exibe apenas os 3 primeiros dígitos do CNPJ, mascarando o restante."""
+    if not cnpj:
+        return ''
+    digits = ''.join(c for c in str(cnpj) if c.isdigit())
+    if len(digits) >= 3:
+        return digits[:3].zfill(3) + '.XXX.XXX/XXXX-XX'
+    return str(cnpj)
